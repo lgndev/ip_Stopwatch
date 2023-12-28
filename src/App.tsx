d5cbd859d9099fc8c700cef0e100bc7356776d78
lgndev <logan.sawyer.dev@gmail.com>
2023-12-28 11:02:51 -0500

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+const App = () => {
+  // build a stopwatch app
+  // start | pause | reset buttons
+  // display elapsed time in seconds
+
+  return (
+    <>
+      <div>stopwatch</div>
+    </>
+  );
+};
+
+export default App;
